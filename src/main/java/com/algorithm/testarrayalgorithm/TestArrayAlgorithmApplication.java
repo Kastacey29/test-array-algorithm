1e04c95e196b041613470c92bf0c1da9422368d3
Stacey29 <Kastacey@yandex.ru>
2022-12-11 15:18:25 +0300

    Initial Commit

---
+++ src/main/java/com/algorithm/testarrayalgorithm/TestArrayAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.algorithm.testarrayalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestArrayAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestArrayAlgorithmApplication.class, args);
+    }
+
+}
